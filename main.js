3a2572581621306a7fe4cfbe89e687f7b9e1fd21
luyvannda <philips.joe329@gmail.com>
2023-08-18 15:26:31 +0700

    define a conditional statements to check if character is alphabetic or not in order to not transform non-alphabetic character

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function rot13(str) {
-  let split = str.split(/[\s+]/g);
-  let strArray = Array.from(split.join(""));
+
+  // turn string into array to use map() method
+  let strArray = Array.from(str);
   console.log(strArray);
 
   let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -9,11 +10,22 @@ function rot13(str) {
     .slice(13)
     .concat(alphabet.slice(0, 13));
 
-  let output = strArray.map(char => String(shiftedAlphabet[
-    alphabet.indexOf(char)]))
+  let output = strArray.map((char) => {
+
+    // Check if the char is alphabetic if true shift the alphabet.
+
+    if (char.toLowerCase() !== char.toUpperCase()) {
+
+      return shiftedAlphabet[alphabet.indexOf(char)];
+
+    } else {
+      // if char is non-alphabetic like white space and punctuation, preserve them.  
+      return char;
+    }
+  });
 
   return output.join("");
-}
+};
 
 let result = rot13("SERR PBQR PNZC");
 console.log(result);
