5983b3bc0d6a1fe55ef32dc852b87c39371c21e4
luyvannda <philips.joe329@gmail.com>
2023-08-18 11:11:18 +0700

    Come closer to the solution, but just need to figure out how to not transform white space when calling the function

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 function rot13(str) {
-  let lowerCase = str.toLowerCase();
-  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let split = str.split(/[\s+]/g);
+  let strArray = Array.from(split.join(""));
+  console.log(strArray);
+
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   const shiftedAlphabet = alphabet
     .slice(13)
     .concat(alphabet.slice(0, 13));
 
-  console.log(shiftedAlphabet);
+  let output = strArray.map(char => String(shiftedAlphabet[
+    alphabet.indexOf(char)]))
 
-
+  return output.join("");
 }
 
 let result = rot13("SERR PBQR PNZC");
 console.log(result);
-
