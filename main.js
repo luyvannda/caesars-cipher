23afb1eb493e3fa0ee2c41a36f005e999797cbaf
luyvannda <philips.joe329@gmail.com>
2023-08-18 10:17:39 +0700

    Use slice to copy the shifted alphabet from 13 index to the beginning and concat the rest from 0 to 12 index (inclusive) to the end

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+function rot13(str) {
+  let lowerCase = str.toLowerCase();
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const shiftedAlphabet = alphabet
+    .slice(13)
+    .concat(alphabet.slice(0, 13));
+
+  console.log(shiftedAlphabet);
+
+
+}
+
+let result = rot13("SERR PBQR PNZC");
+console.log(result);
+
